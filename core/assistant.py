138263fc0df1dc8901c18cc39775ac6f60ab6db3
Joshua Short <joshua.short@gmail.com>
2018-03-25 20:49:48 -0700

    Update assistant signature and logging -- uses core config for now

--- core/assistant.py
+++ core/assistant.py
@@ -9,8 +9,15 @@ logger = logging.getLogger(__name__)
 
 class Assistant:
 
-    def __init__(self, config=None):
-        logger.info("Initializing Assistant")
+    def __init__(self, path=None, config=None):
+        logger.info("Loading Assistant: {}".format(path))
+        self.path = path
+
+        logger.debug("Configuring Assistant: {}".format(str(config)))
         self.config = config if config is not None else {}
+
+        from core import Config
+        self.config = Config(path=self.path)
+
+        logger.debug("Assistant Configured: {}".format(self.config))
         self.history = []
-        logger.debug(self.config)
