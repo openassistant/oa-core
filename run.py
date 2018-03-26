c2393b88ae5f14fdd3ffcbae9cd0c67f55634817
Joshua Short <joshua.short@gmail.com>
2018-03-25 20:33:43 -0700

    Move runtime arg parsing to utility in core -- trying to change how configuration/setup happens

--- run.py
+++ run.py
@@ -9,52 +9,9 @@ logger = logging.getLogger(__name__)
 
 import os
 import signal
-import sys
 import subprocess
 
 
-from argparse import ArgumentParser, Namespace
-
-def _parser(args):
-    parser = ArgumentParser()
-
-    parser.add_argument("-c", "--continuous",
-            action="store_true", dest="continuous", default=False,
-            help="Start interface with 'continuous' listen enabled")
-
-    parser.add_argument("-p", "--pass-words",
-            action="store_true", dest="pass_words", default=False,
-            help="Pass the recognized words as arguments to the shell command")
-
-    parser.add_argument("-H", "--history", type=int,
-            action="store", dest="history",
-            help="Number of commands to store in history file")
-
-    parser.add_argument("-m", "--microphone", type=int,
-            action="store", dest="microphone", default=None,
-            help="Audio input card to use (if other than system default)")
-
-    parser.add_argument("--valid-sentence-command", type=str,
-            dest="valid_sentence_command", action='store', default=None,
-            help="Command to run when a valid sentence is detected")
-
-    parser.add_argument("--invalid-sentence-command", type=str,
-            dest="invalid_sentence_command", action='store', default=None,
-            help="Command to run when an invalid sentence is detected")
-
-    parser.add_argument("-M", "--mind", type=str,
-            dest="mind_dir", action='store',
-            help="Path to mind to use for assistant")
-
-    parser.add_argument("-d", "--debug",
-            action='store_true', dest="debug", default=False,
-            help="Enable debug-level logging")
-
-    parser.add_argument("-u", "--update",
-            action='store_true', dest="update", default=False,
-            help="Update language files online")
-
-    return parser.parse_args(args)
 
 
 def recognizer_finished(a, recognizer, text):
@@ -140,18 +97,16 @@ def process_command(self, command):
 if __name__ == '__main__':
 
     from gi.repository import GObject
-
-    from core import Config, Assistant
+    import sys
 
     # Parse command-line options,
     #  use `Config` to load mind configuration
     #  command-line overrides config file
-    args = _parser(sys.argv[1:])
+    from core.util.args import _parser as arg_parser
+    args = arg_parser(sys.argv[1:])
     if args.debug:
         logging.root.setLevel(logging.DEBUG)
     logger.debug("Arguments: {args}".format(args=args))
-
-    conf = Config(path=args.mind_dir, **vars(args))
 
 
     # Database Prototyping
@@ -208,8 +163,8 @@ if __name__ == '__main__':
 
 
     # A configured Assistant
-    a = Assistant(config=conf)
-    # a.db = db
+    from core import Assistant
+    a = Assistant(path=args.mind_dir)
 
 
     #
