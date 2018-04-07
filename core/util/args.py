ad64f3b92591be9d17c2ab8540a8f77bfff2870e
Joshua Short <joshua.short@gmail.com>
2018-04-07 01:38:42 -0700

    Add agent-loading args

--- core/util/args.py
+++ core/util/args.py
@@ -3,6 +3,13 @@ from argparse import ArgumentParser, Namespace
 def _parser(args):
     parser = ArgumentParser()
 
+    parser.add_argument("-A", "--agents", type=str,
+            dest="agents_dir", action='store',
+            help="Path to agents repository directory")
+
+    parser.add_argument("-a", "--agent", type=str,
+            dest="agent", action="store",
+            help="Agent to load")
 
 
     parser.add_argument("-d", "--debug",
