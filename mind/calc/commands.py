8327a32bc965fb2c008b7958e1c64573673574ef
Verse Uni <akamosko@gmail.com>
2018-04-19 21:49:53 +0300

    calc improvements.
    calc improvements.

--- mind/calc/commands.py
+++ mind/calc/commands.py
@@ -1,20 +1,25 @@
 opers="""
 plus:+
 minus:-
-multiply:*
-divide://
-logarithm:math.log
+multiply,mul:*
+divide,div://
+log:math.log
 pi:math.pi
 power:**
-point:.
+point,dot:.
 square:**0.5
+open:(
+close:)
 """
-sys_info.expr=''
+sys_info.expr=[]
 
 kws={
-  'equal':calculate(),
+  'equal, result':calculate(),
+  'home':say('back to boot') & mind('boot'),
   'quit':quit_app()
 }
 
-kws.update(_lines_to_dict(_fread('nums'), add2expr))
-kws.update(_lines_to_dict(opers,add2expr))
+sys_info.calc_nums=_lines_to_dict(_fread('nums'), add2expr)
+sys_info.calc_opers=_lines_to_dict(opers,add2expr)
+kws.update(sys_info.calc_nums)
+kws.update(sys_info.calc_opers)
