2dc916e63e63f1b5daa032fa75ea49851def91c8
Joshua Short <joshua.short@gmail.com>
2018-06-11 03:38:24 -0700

    Add workaround for mac to use subprocess/say instead of pyttsx3 because of a threading bug (thanks Alex!)

--- parts/voice.py
+++ parts/voice.py
@@ -6,24 +6,33 @@ from core import oa
 from abilities.core import get, put
 
 
+import platform
+sys_os = platform.system()
+flMac = (sys_os != 'mac')
+if not flMac:
+    import pyttsx3
+else:
+    import subprocess
+
+
 def _in():
-    tts = pyttsx3.init()
-    
-    # Set the voice to a German accent.
-    # voices = tts.getProperty('voices')
-    # tts.setProperty('voice', 'german')
-    
-    # Set speaking speed rate.
-    rate = tts.getProperty('rate')
-    tts.setProperty('rate', rate - 28)
+    if not flMac:
+        tts = pyttsx3.init()
 
     while oa.alive:
         s = get()
         # Pause Ear (listening) while talking. Mute TTS.
         put('stt','mute')
 
-        tts.say(s)
-        tts.runAndWait()
+        if flMac:
+            _msg = subprocess.Popen(['echo', s], stdout=subprocess.PIPE)
+            _tts = subprocess.Popen(['say'], stdin=_msg.stdout)
+            _msg.stdout.close()
+            _tts.communicate()
+        else:
+            tts.say(s)
+            tts.runAndWait()
+
         # Wait until speaking ends.
         # Continue ear (listening). Unmute TTS.
         put('stt','unmute')
