a97e77015666b6170ec7eee685cd8e02dc44f795
Joshua Short <joshua.short@gmail.com>
2018-06-19 16:21:10 -0700

    Initial update/fix of abilities for update Core functionality

--- abilities/core.py
+++ abilities/core.py
@@ -1,3 +1,4 @@
+import logging
 import threading
 
 from core import oa, queue, Stub
@@ -13,12 +14,12 @@ def thread_name():
 def current_part():
     """ Return the part name which is associated with the current thread. """
     name = thread_name()
-    ret = oa.core.parts.get(name, None)
-    if ret is None:
+    if hasattr(oa.core.parts, name):
+        return oa.core.parts[name]
+    else:
         err = '%s Error: Cannot find a related part' %name
-        info(err)
+        logging.error(err)
         raise Exception(err)
-    return ret
 
 def call_function(func_or_value):
     """ A helper function. For Stubs, call `perform()`.
@@ -38,8 +39,8 @@ def info(*args, **kwargs):
         string += ' '.join([str(v) for v in args]) + '\n'
     if kwargs:
         string += '\n'.join([' %s: %s' %(str(k), str(v)) for k, v in kwargs.items()])
-    if oa.console and not oa.core.finished.is_set():
-        oa.console.wire_in.put(string)
+    if hasattr(oa.core.parts, 'console') and not oa.core.finished.is_set():
+        oa.core.parts.console.wire_in.put(string)
     else:
         print(string)
 
@@ -56,7 +57,7 @@ def get(part = None, timeout = .1):
 
 def put(part, value):
     """ Put a message on the wire. """
-    oa[part].wire_in.put(value)
+    oa.core.parts[part].wire_in.put(value)
 
 def empty(part = None):
     """ Remove all messages from `part.wire_in` input queue.
