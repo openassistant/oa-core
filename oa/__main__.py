466de082849aa8cc1d8cc3d2a20591c8a8e1916f
Joshua Short <joshua.short@gmail.com>
2018-10-14 00:55:49 -0700

    little better handling of log template and debug mode

--- oa/__main__.py
+++ oa/__main__.py
@@ -90,7 +90,8 @@ if __name__ == '__main__':
     from oa.util.args import _parser
     args = _parser(sys.argv[1:])
     
-    logging.basicConfig(level=logging.DEBUG, filename=args.log_file, format="[%(asctime)s] %(levelname)-8s %(threadName)-10s [%(filename)s:%(funcName)s:%(lineno)d]    %(message)s")
+    log_template = "[%(asctime)s] %(threadName)s [%(filename)s:%(funcName)s:%(lineno)d] %(levelname)s: %(message)s"
+    logging.basicConfig(level=logging.INFO if not args.debug else logging.DEBUG, filename=args.log_file, format=log_template)
     logging.info("Start Open Assistant")
 
     start()
