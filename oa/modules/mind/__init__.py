ca3876df5f6b497eb5128f476da1a60c235ff503
Joshua Short <joshua.short@gmail.com>
2018-12-28 13:31:18 -0800

    Change core namespace import

--- oa/modules/mind/__init__.py
+++ oa/modules/mind/__init__.py
@@ -4,7 +4,7 @@ import importlib
 import logging
 import os
 
-from oa.core import oa
+from oa import core
 from oa.core.util import Core, isCallable
 from oa.modules.abilities.core import info, call_function, get
 from oa.modules.abilities.system import read_file, sys_exec
@@ -22,7 +22,7 @@ def load_mind(path):
     mind = Core()
     mind.module = path
     mind.name = os.path.splitext(os.path.basename(mind.module))[0]
-    mind.cache_dir = os.path.join(oa.core_directory, 'cache', mind.name)
+    mind.cache_dir = os.path.join(core.oa.core_directory, 'cache', mind.name)
 
     # Make directories.
     if not os.path.exists(mind.cache_dir):
@@ -46,8 +46,8 @@ def set_mind(name, history=True):
     if history:
         _history.append(name)
         
-    oa.core.mind = oa.core.minds[name]
-    return oa.core.mind
+    core.oa.core.mind = core.oa.core.minds[name]
+    return core.oa.core.mind
 
 def switch_back():
     """ Switch back to the previous mind. (from `switch_hist`). """
@@ -62,7 +62,7 @@ def load_minds():
         if mind.lower().endswith('.py'):
             logging.info("<- {}".format(mind))
             m = load_mind(os.path.join(mind_path, mind))
-            oa.core.minds[m.name] = m
+            core.oa.core.minds[m.name] = m
     logging.info('Minds loaded!')
 
 def _in():
@@ -74,10 +74,10 @@ def _in():
     logging.debug('"{}" is now listening. Say "Boot Mind!" to see if it can hear you.'.format(default_mind))
 
 
-    while not oa.core.finished.is_set():
+    while not core.oa.core.finished.is_set():
         text = get()
         logging.debug('Input: {}'.format(text))
-        mind = oa.core.mind
+        mind = core.oa.core.mind
         if (text is None) or (text.strip() == ''):
             # Nothing to do.
             continue
