63b054069e395fe2577c7f72944150abdcfa8721
Thomas Rhoads <Thomas.Rhoads@lfg.com>
2020-08-27 14:04:33 -0400

    PEP8

--- oa/util/args.py
+++ oa/util/args.py
@@ -1,18 +1,19 @@
 from argparse import ArgumentParser, Namespace
 
+
 def _parser(args):
     parser = ArgumentParser()
 
     parser.add_argument("-c", "--config",
-            dest='config_file', action='store', default=None,
-            help="Path to config file")
+                        dest='config_file', action='store', default=None,
+                        help="Path to config file")
 
     parser.add_argument("-d", "--debug",
-            action='store_true', dest="debug", default=False,
-            help="Enable debug-level logging")
+                        action='store_true', dest="debug", default=False,
+                        help="Enable debug-level logging")
 
     parser.add_argument("-l", "--log",
-            dest='log_file', action='store', default=None,
-            help="Path to log file")
+                        dest='log_file', action='store', default=None,
+                        help="Path to log file")
 
     return parser.parse_args(args)
